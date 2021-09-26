52eb3daef13751b252672f7d35bf8426632d44c7
MaksimKuzur <m.lakatkou@gmail.com>
2021-09-26 12:23:05 +0300

    made adjustments to lesson 2

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 const mainMenuList = {
     css: "mainmenu_list",
-    view:"list",
-    id:"mainMenuList",
-    // minWidth:150,
+    view: "list",
+    id: "mainMenuList",
     borderless: true,
-    autoheight:true,
-    scroll:false,
-    data:[ "Dashboard", "Users", "Products", "Locations" ]
+    autoheight: true,
+    scroll: false,
+    data: [ "Dashboard", "Users", "Products", "Locations" ]
 };
 
 const mainMenuLabel = {
-    template:"<span class='webix_icon wxi-check'></span>Connected",
+    template: "<span class='webix_icon wxi-check'></span>Connected",
     borderless: true,
-    width:150,
-    height:30,
-    css:"mainmenu_label"
+    width: 150,
+    height: 30,
+    css: "mainmenu_label"
 };
 
 const mainMenu = {
-    css:"mainmenu_spaser",
-    rows:[
+    css: "mainmenu_spaser",
+    rows: [
         mainMenuList,
         { },
         mainMenuLabel
@@ -27,122 +26,105 @@ const mainMenu = {
 };
 
 const dataTable = {
-    view:"datatable",
-    id:"dataTable",
-    scrollX:false,
-    css:"datatable_scrollbar_x",
-    data:small_film_set,
-    autoConfig:true,
+    view: "datatable",
+    id: "dataTable",
+    scrollX: false,
+    css: "datatable_scrollbar_x",
+    data: small_film_set,
+    autoConfig: true,
 };
 
 const formToolbar = {
-    margin:10,
-    paddingX:15,
-    borderless:true,
-    cols:[
+    margin: 10,
+    borderless: true,
+    cols: [
         {
-            view:"button",
-            value:"Add new",
-            css:"webix_primary",
-            click:function() {
-                if($$("formInputElements").validate()){
-                    let item = $$("formInputElements").getValues();
+            view: "button",
+            value: "Add new",
+            css: "webix_primary",
+            click() {
+                if($$("formForDatatable").validate()){
+                    let item = $$("formForDatatable").getValues();
                     $$("dataTable").add(item);
-                    webix.message("validation is successful");
+                    webix.message("A new record has been added to the table");
                 }
             }
         },
         {
-            view:"button",
-            value:"Clear",
-            click:function(){
+            view: "button",
+            value: "Clear",
+            click() {
                 webix.confirm({
-                    text:"Clear the form?"
+                    text: "Clear the form?"
                 }).then(
-                    function(){
+                    () => {
                         webix.message("Confirmed");
-                        $$("formInputElements").clearValidation();
-                        $$("formInputElements").clear();
+                        $$("formForDatatable").clearValidation();
+                        $$("formForDatatable").clear();
                     }, 
-                    function(){
-                        webix.message("Rejected");
-                    }
+                    () => webix.message("Rejected")
                 );
             }
         }
     ]
 };
 
-const formInputElements = {
+const formForDatatable = {
     borderless: true,
-    view:"form",
-    id: "formInputElements",
-    width:250,
-    elements:[
-        { view:"text", label:"Title", name:"title", invalidMessage:"“title“ must be filled in" },
-        { view:"text", label:"Year", name:"year", invalidMessage:"“year“ should be between 1970 and current" },
-        { view:"text", label:"Votes", name:"votes", invalidMessage:"“votes“ must be less than 100000" },        
-        { view:"text", label:"Rating", name:"rating", invalidMessage:"“rating“ cannot be empty or 0" },
-        { view:"text", label:"Rank", name:"rank" },
-        { view:"text", label:"Category", name:"category" },
+    view: "form",
+    id: "formForDatatable",
+    width: 250,
+    elements: [
+        {
+            view: "template",
+            template: "Edit films",
+            type: "section",
+        },
+        { view: "text", label: "Title", name: "title", invalidMessage: "must be filled in" },
+        { view: "text", label: "Year", name: "year", invalidMessage: "1970 < \"year\" < current" },
+        { view: "text", label: "Votes", name: "votes", invalidMessage: "< 100000" },        
+        { view: "text", label: "Rating", name: "rating", invalidMessage: "cannot be empty or 0" },
+        { view: "text", label: "Rank", name: "rank" },
+        { view: "text", label: "Category", name: "category" },
+        formToolbar,
+        { }
     ],
-    rules:{
-        title:webix.rules.isNotEmpty,
-        year:function(value){
-            return value>1970 && value <(new Date()).getFullYear();
-        },
-        votes:function(value){
-            return value>0 && value < 100000;
-        },
-        rating:function(value){ return value > 0; },
+    rules: {
+        title: webix.rules.isNotEmpty,
+        year: value => value > 1970 && value < (new Date()).getFullYear(),
+        votes: value => value > 0 && value < 100000,
+        rating: value => value > 0,
     }
 };
 
-const formForDatatable = {
-    rows:[
-        {
-            view:"fieldset",
-            label:"Edit films",
-            height:50,
-            css: "form_title"
-        },
-        formInputElements,
-        formToolbar,
-        { }
-    ]
-};
-
 const content = { 
-    cols:[ 
+    cols: [ 
         mainMenu,
-        { view:"resizer"},
+        { view: "resizer"},
         dataTable,
         formForDatatable
     ]
 };
 
 const headerButton = {
-    view:"button",
-    id:"headerButton",
-    type:"icon",
-    icon:"mdi mdi-account-search",
-    label:"Profile",
-    click:function(){
-        $$("popupProfile").show({
-            x:(window.screen.width-135),
-            y:40
-        });
+    view: "button",
+    id: "headerButton",
+    type: "icon",
+    icon: "mdi mdi-account-search",
+    label: "Profile",
+    click() {
+        $$("popupProfile").show($$("headerButton").getNode());
     },
-    css:"header_button",
-    width:100,
+    css: "header_button",
+    width: 100,
 };
 
 const header = {
-    css:"header",
-    cols:[ 
+    css: "header",
+    cols: [ 
         {
-            template:"My App",
-            css:"header_label",
+            template: "My App",
+            css: "header_label",
             borderless: true
         },
         headerButton,
@@ -150,13 +132,13 @@ const header = {
 };
 
 const footer = {
-    css:"footer",
-    template:"The sofware is provided by <a href='URL'>https://webix.com</a>. All rights reserved (c)",
+    css: "footer",
+    template: "The sofware is provided by <a href='URL'>https://webix.com</a>. All rights reserved (c)",
     height: 30
 };
 
 webix.ui({
-    rows:[
+    rows: [
         header,
         content,
         footer,
@@ -164,13 +146,11 @@ webix.ui({
 });
 
 webix.ui({
-    view:"popup",
-    id:"popupProfile",
-    move:true,
-    // position:"center",
-    body:{
-      view:"list",
-      autoheight:true,
-      data:[ "Settings", "Log Out" ]
+    view: "popup",
+    id: "popupProfile",
+    body: {
+      view: "list",
+      autoheight: true,
+      data: [ "Settings", "Log Out" ]
     }
   });
